6e5a888b307dd3fe4b6642a18943bf68e2613012
Omar Wael <omarinoowael@gmail.com>
2026-04-23 18:14:29 +0200

    Add Loader component and styles; enhance project card hover effect

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/NavBar/navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Banner from './Components/banner/banner'
-import Skills from './Components/Skills/skills';
-import Projects from './Components/tabs/tabs';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/NavBar/navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Banner from "./Components/banner/banner";
+import Skills from "./Components/Skills/skills";
+import Projects from "./Components/tabs/tabs";
+import Loader from "./Components/loader/loader";
 function App() {
   return (
-    <><Navbar /><Banner /><Skills /><Projects /></>
+    <>
+      <Loader />
+      <Navbar />
+      <Banner />
+      <Skills />
+      <Projects />
+    </>
   );
 }
 
