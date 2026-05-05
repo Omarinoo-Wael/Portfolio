5e64c4a1e1eae72530dcc1a365143f6cec94d5c7
Omar Wael <omarinoowael@gmail.com>
2026-05-06 01:36:00 +0300

    Refactor styles and improve layout in Skills and Banner components; add profile image

--- myportfolio/src/Components/banner/banner.js
+++ myportfolio/src/Components/banner/banner.js
@@ -4,51 +4,31 @@ import myCV from "../../assets/CV.pdf";
 import downloadImg from "../../assets/download.png";
 import "./style.css";
 import React, { useState, useEffect } from "react";
+import Image from "react-bootstrap/Image";
+import profilePic from "../../assets/me.jpeg";
 
 function Banner() {
-  const [text, setText] = useState("");
-  const [finished, setFinished] = useState(false);
-  const fulltext = "OMAR WAEL";
-
-  useEffect(() => {
-    let i = 0;
-    const interval = setInterval(() => {
-      setText(fulltext.substring(0, i + 1));
-      i++;
-      if (i >= fulltext.length) {
-        clearInterval(interval);
-        setFinished(true);
-      }
-    }, 150);
-
-    return () => clearInterval(interval);
-  }, []);
   return (
     <section className="banner" id="home">
       <Container>
+        <Row className="justify-content-center">
+          <Image className="me" src={profilePic} roundedCircle />
+        </Row>
         <Row>
           <Col>
             <span>
-              <h1>
-                {text}
-                <span className={finished ? "finished" : "cursor"}>|</span>
-              </h1>
+              <h1>OMAR WAEL</h1>
             </span>
             <p>
               Full-stack developer with a Backend heart and a Game Dev soul.
               <br />I focus on building high-performance systems and immersive
               digital environments from the ground up.
             </p>
-            <button className="cv-btn" onClick={() => window.open(myCV)}>
-              Download CV
-              <img src={downloadImg} alt="" />
-            </button>
-          </Col>
-          <Col>
-            <div className="profile-card">
-              <div className="guy">
-                <img src={require("../../assets/guy.png")} alt="Guy" />
-              </div>
+            <div className="btn-holder">
+              <button className="cv-btn" onClick={() => window.open(myCV)}>
+                Download CV
+                <img src={downloadImg} alt="" />
+              </button>
             </div>
           </Col>
         </Row>
